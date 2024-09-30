51e5b9960e8db70444046593a33c73608c22a415
Moizkazmi245 <mujtabakazmi0321@gmail.com>
2024-09-30 16:29:32 +0500

    sds

--- homepage/app.js
+++ homepage/app.js
@@ -1,14 +1,53 @@
-function add() {
-    let pricev = document.getElementById('price').innerText;
-    let pnv = parseFloat(pricev);
-    let npv = pnv + 250
-    document.getElementById('price').innerText = npv;
-    let cartv = document.getElementById('citems').innerText;
-    let cartnv = +cartv
-    let ncv = cartnv + 1
-    document.getElementById('citems').innerText = ncv ;
+
+let totalItems = 0; 
+let totalPrice = 0.0; 
+const cartContent = document.querySelector('.cart-content');
+
+function addToCart(price, imgSrc, productName) {
+    totalItems++;
+    totalPrice += price;
+
+    document.getElementById('citems').innerText = totalItems;
+    document.getElementById('price').innerText = totalPrice.toFixed(2);
+
+    const productDiv = document.createElement('div');
+    productDiv.classList.add('cart-product');
+    productDiv.innerHTML = `
+        <img src="${imgSrc}" alt="${productName}" class="cart-product-img" />
+        <div class="cart-product-details">
+            <h4>${productName}</h4>
+            <p>Price: $${price}</p>
+        </div>
+    `;
+
+    cartContent.appendChild(productDiv);
+
+    if (cartContent.children.length === 1) {
+        cartContent.innerHTML = ''; 
+    }
 }
+
+
 function removeall() {
-    document.getElementById('price').innerText = "0.00";
-    document.getElementById('citems').innerText = 0 ;
+    totalItems = 0;
+    totalPrice = 0.0;
+    document.getElementById('citems').innerText = totalItems;
+    document.getElementById('price').innerText = totalPrice.toFixed(2);
+
+    cartContent.innerHTML = '<p>Cart is empty</p>';
 }
+
+const cartToggleBtn = document.getElementById('cartToggleBtn');
+const closeBtn = document.getElementById('closeBtn');
+const cartSidebar = document.getElementById('cart');
+const overlay = document.getElementById('overlay');
+
+
+  function toggleCart() {
+    cartSidebar.classList.toggle('active');
+    overlay.classList.toggle('active');
+  }
+  
+  cartToggleBtn.addEventListener('click', toggleCart);
+  closeBtn.addEventListener('click', toggleCart);
+  overlay.addEventListener('click', toggleCart);
